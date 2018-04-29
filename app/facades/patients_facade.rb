39e878f54da95ba10f8ec2ac3d56212928dc97be
Khateeb Ahmad <x16112989@student.ncirl.ie>
2018-04-29 22:52:20 +0100

    Facade design pattern

---
+++ app/facades/patients_facade.rb
@@ -0,0 +1,17 @@
+class PatientsFacade
+
+    def index
+        Patient.index
+    end
+
+    def list
+        Patient.list
+    end
+
+    def new
+        Patient.new
+    end
+
+    def create
+        Patient.create
+    end
